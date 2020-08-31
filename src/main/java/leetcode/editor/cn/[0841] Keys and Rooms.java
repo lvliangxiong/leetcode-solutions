c3c8dfb561f688ccc3764fba8e1957d151b4a5c8
JoeyLyu <lvliangxiong@foxmail.com>
2020-08-31 10:01:45 +0800

    dfs

---
+++ src/main/java/leetcode/editor/cn/[0841] Keys and Rooms.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * <pre>
+ * There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may
+ * have some keys to access the next room.
+ *
+ * Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where
+ * N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+ *
+ * Initially, all the rooms start locked (except for room 0).
+ *
+ * You can walk back and forth between rooms freely.
+ *
+ * Return true if and only if you can enter every room.
+ *
+ * Example 1:
+ *
+ * Input: [[1],[2],[3],[]]
+ * Output: true
+ * Explanation:
+ * We start in room 0, and pick up key 1.
+ * We then go to room 1, and pick up key 2.
+ * We then go to room 2, and pick up key 3.
+ * We then go to room 3.  Since we were able to go to every room, we return true.
+ *
+ * Example 2:
+ *
+ * Input: [[1,3],[3,0,1],[2],[0]]
+ * Output: false
+ * Explanation: We can't enter the room with number 2.
+ *
+ * Note:
+ *
+ *     1 <= rooms.length <= 1000
+ *     0 <= rooms[i].length <= 1000
+ *     The number of keys in all rooms combined is at most 3000.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/keys-and-rooms
+ * </pre>
+ */
+class KeysAndRooms {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        int count = 0;
+
+        public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+            boolean[] visited = new boolean[rooms.size()];
+            dfs(0, visited, rooms);
+            return count == visited.length;
+        }
+
+        private void dfs(int start, boolean[] visited, List<List<Integer>> rooms) {
+            if (visited[start]) return;
+            visited[start] = true;
+            count++;
+            for (Integer key : rooms.get(start)) {
+                dfs(key, visited, rooms);
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
